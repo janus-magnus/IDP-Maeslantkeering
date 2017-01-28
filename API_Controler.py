32e36fd9352de77abecb6a8057c14de6bdb7cd40
janus-magnus <Ykorringa@gmail.com>
2017-01-28 15:20:19 +0100

    API rewrite + base monitoring system

--- API_Controler.py
+++ API_Controler.py
@@ -3,18 +3,18 @@ from pyowm import *
 W_API_key = '0292ef38878076e5a546762d1c2a5ff8'  # weather API key
 owmAPI = OWM(W_API_key)
 owm = owmAPI.weather_at_place('Utrecht,NL')
-weather_ob = owm.get_weather()
+w = owm.get_weather()
+
 
 '''def weather():
     owm = owmAPI.weather_at_place('Utrecht,NL')
     weather_ob = owm.get_weather()
     return weather_ob
 '''
-def get_rain():
-    rain = weather_ob.get_rain()
+def get_rain():# zou het volume van regen in de laatste 3 uur moeten weer geven maar blijft nu nog leeg geen idee waarom
+    rain = w.get_rain()
     return rain
 
 def get_wind():
-    wind = weather_ob.get_wind()
-    return wind
-
+    wind = w.get_wind()
+    return wind['speed']
--- monitor_system.py
+++ monitor_system.py
@@ -1,37 +1,46 @@
 import time
 import gate_control_functions as gc
+import API_Controler as APIC
+import sensor_controler as sc
 
-#warning flags
+'''
 touchWarning = False
 tempWarning = False
 humidityWarning = False
 windSpeedWarning = False
 rainWarning = False
+'''
 
 #treshholds Oranje
-tempTreshold = 25
-humTreshold = 25
-windSpeedTreshold = 50
-rainTreshold = 10
+tempTresholdO = 25
+humTresholdO = 35
+windSpeedTresholdO = 30
+rainTresholdO = 5
 
 #thresholds rood
-tempTreshold = 30
-humTreshold = 30
-windSpeedTreshold = 60
-rainTreshold = 15
+tempTresholdR = 30
+humTresholdR = 50
+windSpeedTresholdR = 50
+rainTresholdR = 10
 
-while True:#placeholder loop moet werkend worden gemaakt
-    if GPIO.input(36)==1:
-        touchWarning=True
-        continue
-    if humidity >= humTreshold:
-        humidityWarning = True
-        continue
-    if temperature >= tempTreshold:
-        tempWarning=True
-        continue
-    if touchWarning == True and tempWarning== True and humidityWarning == True:
-        gc.open_gate()
+def define_threat_level():
+    threatCount = 0
 
-def get_colour_code():
-    # count de flags die op true staan en geeft een kleur code terug
+    if sc.get_touch()==1:
+        threatCount+=2
+    if sc.get_humidity() >= humTresholdO:
+        threatCount+=1
+    if sc.get_humidity() >= humTresholdR:
+        threatCount+=1
+    if sc.get_temp() >= tempTresholdO:
+        threatCount += 1
+    if sc.get_temp() >= tempTresholdR:
+        threatCount += 1
+    if sc.get_wind() >= windSpeedTresholdO:
+        threatCount += 1
+    if sc.get_wind() >= windSpeedTresholdR:
+        threatCount += 1
+    #regen moet er nog bij maar het werkt nog niet helemaal
+
+    #bij threatcount>4 oranje bij >6 rood, dit gebeurt in de screen clas
+    return threatCount
