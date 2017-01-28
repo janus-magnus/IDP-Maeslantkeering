f6ce3b9d2d78eba3a3d13ebca4ec944f9f368bdc
janus-magnus <Ykorringa@gmail.com>
2017-01-28 10:47:20 +0100

    sensor controler update

--- sensor_controler.py
+++ sensor_controler.py
@@ -7,15 +7,29 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(36, GPIO.IN)# touch sensor input
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)# temp en hum sensor input
-humidity = round(humidity, 2)
-temperature = round(temperature, 2)
+
+
+def get_rawTempHum():
+    global humidity, temperature
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)  # temp en hum sensor input
+
 
 def get_humidity():
+    global humidity
+    get_rawTempHum()
+    humidity = round(humidity, 2)
     return humidity
 
 def get_temp():
+    global temperature
+    get_rawTempHum()
+    temperature = round(temperature, 2)
     return temperature
 
 def get_touch():
     return GPIO.input(36)
+
+
+print(''+get_humidity() +' + ' + get_temp())
+print(''+get_humidity() +' + ' + get_temp())
+print(''+get_humidity() +' + ' + get_temp())
