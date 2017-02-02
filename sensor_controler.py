aa6842d921c89dd8ca139999242e09fdccee54f6
janus-magnus <ykorringa@gmail.com>
2017-02-02 13:24:59 +0100

    uncomment voor pi version

--- sensor_controler.py
+++ sensor_controler.py
@@ -1,14 +1,14 @@
-# import Adafruit_DHT
-# import RPi.GPIO as GPIO
-#
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(36, GPIO.IN)# touch sensor input
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+ GPIO.setmode(GPIO.BOARD)
+ GPIO.setup(36, GPIO.IN)# touch sensor input
 humidity = 0
 temperature = 0
 
 def get_rawTempHum():
     global humidity, temperature
-    # humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)  # temp en hum sensor input
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)  # temp en hum sensor input
 
 def get_humidity():
     global humidity
